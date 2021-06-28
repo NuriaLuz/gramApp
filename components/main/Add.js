97544c9be938c86f59e3df71c5cad06922a7e743
Nuria Rodriguez <nurialuzr1@gmail.com>
2021-06-28 03:39:57 -0400

    camera can take pics and pick images from phone

--- components/main/Add.js
+++ components/main/Add.js
@@ -1,31 +1,65 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet,Text, View, TouchableOpacity ,  Button} from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 import { Camera } from 'expo-camera';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function Add() {
-    const [hasPermission, setHasPermission] = useState(null);
+    const [hasCameraPermission, setHasCameraPermission] = useState(null);
+    const [hasGalleryPermission, setHasGalleryPermission] = useState(null);
     const [type, setType] = useState(Camera.Constants.Type.back);
+    const [camera, setCamera] = useState(null);
+    const [image, setImage] = useState(null);
 
     useEffect(() => {
         (async () => {
-            const { status } = await Camera.requestPermissionsAsync();
-            setHasPermission(status === 'granted');
+            const cameraStatus = await Camera.requestPermissionsAsync();
+            setHasCameraPermission(cameraStatus.status === 'granted');
+
+            const gallaryStatus = await ImagePicker.requestMediaLibraryPermissionsAsync();
+            setHasGalleryPermission(gallaryStatus.status === 'granted')
+
         })();
     }, []);
 
-    if (hasPermission === null) {
+
+    const takePicture = async () => {
+        if (camera) {
+            const data = await camera.takePictureAsync(null)
+            setImage(data.uri);
+        }
+    }
+
+    const pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect: [1, 1],
+            quality: 1,
+        });
+
+        console.log(result);
+
+        if (!result.cancelled) {
+            setImage(result.uri);
+        }
+    };
+
+
+
+    if (hasCameraPermission === null || hasGalleryPermission === false) {
         return <View />;
     }
-    if (hasPermission === false) {
+    if (hasCameraPermission === false || hasGalleryPermission === false) {
         return <Text>No access to camera</Text>;
     }
     return (
-        <View>
+        <View style={{ flex: 1 }}>
             <View style={styles.cameraContainer}>
-                <Camera 
-                style={styles.fixedRation} 
-                type={type} 
-                ratio={'1:1'}
+                <Camera
+                    ref={ref => setCamera(ref)}
+                    style={styles.fixedRation}
+                    type={type}
+                    ratio={'1:1'}
                 />
             </View>
 
@@ -39,6 +73,15 @@ export default function Add() {
                     );
                 }}>
             </Button>
+
+            <Button
+                title="Take Picture"
+                onPress={() => takePicture()} />
+
+            <Button
+                title="Pick Image From Gallery"
+                onPress={() => pickImage()} />
+            {image && <Image source={{ uri: image }} style={{ flex: 1 }} />}
         </View>
     );
 }
@@ -51,6 +94,6 @@ const styles = StyleSheet.create({
     },
     fixedRation: {
         flex: 1,
-        aspectRatio:1
+        aspectRatio: 1
     }
 })
